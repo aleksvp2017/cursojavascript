c0b41f9063654d3faa273c7bb5c8ac36aae85f69
Aleksander Velozo Pascoal <avelozo@sof.intra>
2018-04-04 12:00:36 -0300

    Funcoes de limpar e criar Negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,17 +7,30 @@ class NegociacaoController{
         this._valor = $('#valor');
     }
 
-    adiciona(event){
+    adicionar(event){
         event.preventDefault();
         
         console.log(this._data.value);
 
+        let negociacao = this.criarNegociacao();
+        console.log(negociacao);
+
+        this.limparFormulario();
+        console.log(negociacao);
+    }
+
+    criarNegociacao(){
         let negociacao = new Negociacao(
             new Date(this._data.value.replace(/-/g, ',')), 
             this._quantidade.value, 
             this._valor.value);
-        
-        console.log(negociacao);
+        return negociacao;
+    }
+
+    limparFormulario(){
+        this._data.value = '';
+        this._quantidade = 0;
+        this._valor = 0.0;
     }
     
 }
